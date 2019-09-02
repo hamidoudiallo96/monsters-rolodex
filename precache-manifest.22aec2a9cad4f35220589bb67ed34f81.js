79237d689de2fe4d32ded13bfc430529f9e2395e
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-09-02 02:38:09 -0400

    Updates

---
+++ precache-manifest.22aec2a9cad4f35220589bb67ed34f81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b4447f10c3a62f18f2892e7da7a566d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "657252b9c82a57efc7fc",
+    "url": "/monsters-rolodex/static/css/main.8a00a8d7.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "657252b9c82a57efc7fc",
+    "url": "/monsters-rolodex/static/js/main.7c40d6dc.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
